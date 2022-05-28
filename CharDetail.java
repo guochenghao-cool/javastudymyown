7f4dc6b4214d807fe5312c14bb0cca55138c5e8d
guochenghao-cool <74386650+guochenghao-cool@users.noreply.github.com>
2022-05-29 00:02:56 +0800

    Add files via upload

---
+++ CharDetail.java
@@ -0,0 +1,14 @@
+public class CharDetail{
+	public static void main(String[] args){
+		char a1='a';
+		System.out.println((int)a1);//输出a对应的ASCII码的值97
+		char a2=97;
+		System.out.println(a2);//输出对应的ASCII码的字母
+		char a3='郭';
+		System.out.println((int)a3);//37101 
+		char a4=37101;
+		System.out.println(a4);//郭
+		char a5='\'';
+		System.out.println((int)a5);
+	}
+}
